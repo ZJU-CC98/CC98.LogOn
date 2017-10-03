de4332e8373a98a82ab4ef198beb3b25658f131f
Iris Sakura <sgjsakura@hotmail.com>
2017-10-04 00:41:32 +0800

    添加 CORS 选项

---
+++ CC98.LogOn/CC98.LogOn/Migrations/20171003162932_AddAllowedCors.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CC98.LogOn.Migrations
+{
+    public partial class AddAllowedCors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AllowedCorsOrigins",
+                table: "Apps",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowedCorsOrigins",
+                table: "Apps");
+        }
+    }
+}
