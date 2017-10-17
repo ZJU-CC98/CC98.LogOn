3739a6271223e913db05eeacc755bad968a99c3f
Iris Sakura <sgjsakura@hotmail.com>
2017-10-17 20:15:55 +0800

    浙大通行证绑定

---
+++ CC98.LogOn/CC98.LogOn/Controllers/ZjuInfoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CC98.LogOn.ZjuInfoAuth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC98.LogOn.Controllers
+{
+	/// <summary>
+	/// 提供浙大通行证相关的操作。
+	/// </summary>
+	public class ZjuInfoController : Controller
+	{
+		/// <summary>
+		/// 请求使用浙大通行证登录。
+		/// </summary>
+		/// <param name="returnUrl">登录 URL。</param>
+		/// <returns>登录后的返回地址。</returns>
+		public IActionResult LogOn(string returnUrl)
+		{
+			var authProperties = new AuthenticationProperties
+			{
+				RedirectUri = Url.Action("LogOnCallback", "ZjuInfo", new { returnUrl })
+			};
+
+			return Challenge(authProperties, ZjuInfoOAuthDefaults.AuthenticationScheme);
+		}
+
+
+		public IActionResult LogOnCallback(string returnUrl)
+		{
+			if (!Url.IsLocalUrl(returnUrl))
+			{
+				returnUrl = Url.Action("Index", "Home");
+			}
+
+			return Redirect(returnUrl);
+		}
+	}
+}
--- CC98.LogOn/CC98.LogOn/Utility.cs
+++ CC98.LogOn/CC98.LogOn/Utility.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Extensions;
 using JetBrains.Annotations;
 
 namespace CC98.LogOn
@@ -9,22 +14,61 @@ namespace CC98.LogOn
 	/// <summary>
 	/// 提供辅助方法。该类型为静态类型。
 	/// </summary>
-    public static class Utility
-    {
+	public static class Utility
+	{
 		/// <summary>
 		/// 获取异常的最终错误消息。
 		/// </summary>
 		/// <param name="ex">异常对象。</param>
 		/// <returns>异常对象的最终错误消息。</returns>
-	    public static string GetMessage([NotNull] this Exception ex)
-	    {
-		    if (ex == null)
-		    {
-			    throw new ArgumentNullException(nameof(ex));
-		    }
+		public static string GetMessage([NotNull] this Exception ex)
+		{
+			if (ex == null)
+			{
+				throw new ArgumentNullException(nameof(ex));
+			}
 
-		    return ex.GetBaseException().Message;
-	    }
+			return ex.GetBaseException().Message;
+		}
 
-    }
+
+
+		/// <summary>
+		/// 判断用户主体是否通过了给定验证方法的验证。
+		/// </summary>
+		/// <param name="principal">要验证的用户主体。</param>
+		/// <param name="authenticationScheme">要判断的验证方法。</param>
+		/// <returns>如果 <paramref name="principal"/> 对象的标识中具有 <paramref name="authenticationScheme"/> 所定义的验证信息，则为 <c>true</c>；否则为 <c>false</c>。</returns>
+		public static bool IsAuthenticatedWith(this ClaimsPrincipal principal, string authenticationScheme)
+		{
+			return principal.Identities.Any(i => i.TryGetAuthenticationMethod() == authenticationScheme);
+		}
+
+		/// <summary>
+		/// 获取用户主体在给定验证方法下的用户名。
+		/// </summary>
+		/// <param name="principal">要获取用户名的主体对象。</param>
+		/// <param name="authenticationScheme">给定的验证方法。</param>
+		/// <returns>获取 <paramref name="principal"/> 对象中在 <paramref name="authenticationScheme"/> 方法下定义的用户名。如果未找到该信息，则返回 <c>null</c>。</returns>
+		public static string GetNameWith(this ClaimsPrincipal principal, string authenticationScheme)
+		{
+			return principal.Identities.FirstOrDefault(i => i.TryGetAuthenticationMethod() == authenticationScheme)?.GetName();
+		}
+
+		/// <summary>
+		/// 尝试找出某个用户标识的 IdentityServer 验证方法。如果失败则返回 <c>null</c>。
+		/// </summary>
+		/// <param name="identity">用户标识对象。</param>
+		/// <returns>用户标识中 IdentityServer</returns>
+
+		private static string TryGetAuthenticationMethod(this IIdentity identity)
+		{
+			if (identity is ClaimsIdentity claimsIdentity)
+			{
+				return claimsIdentity.FindFirst(JwtClaimTypes.AuthenticationMethod)?.Value;
+			}
+
+			return null;
+		}
+	}
 }
