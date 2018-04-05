5fafdb73d54bcdbd989da5a887c5b81657c84ffb
Iris Sakura <sgjsakura@hotmail.com>
2018-04-05 12:26:45 +0800

    浙大通行证功能

---
+++ CC98.LogOn/CC98.LogOn/Controllers/ManageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC98.LogOn.Controllers
+{
+	/// <summary>
+	/// 提供管理相关功能。
+	/// </summary>
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
