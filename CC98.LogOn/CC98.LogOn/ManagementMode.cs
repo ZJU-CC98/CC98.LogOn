fb00e0cbc2b7c38e6d276e57d144df5e3d25f1c4
Iris Sakura <sgjsakura@hotmail.com>
2018-05-27 22:20:29 +0800

    管理界面

---
+++ CC98.LogOn/CC98.LogOn/ManagementMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC98.LogOn
+{
+
+	/// <summary>
+	/// 定义管理功能的模式。
+	/// </summary>
+    public enum ManagementMode
+    {
+		/// <summary>
+		/// 不具有任何管理功能。
+		/// </summary>
+		None = 0,
+		/// <summary>
+		/// 自我管理功能。
+		/// </summary>
+		Self,
+		/// <summary>
+		/// 管理员管理功能。
+		/// </summary>
+		Admin
+    }
+}
